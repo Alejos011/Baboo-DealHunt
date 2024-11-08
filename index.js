5028118d7ecb4bf633085662e5b8800f5b509abb
Alejos011 <138871929+Alejos011@users.noreply.github.com>
2024-11-08 09:15:33 -0600

    .

--- index.js
+++ index.js
@@ -4,20 +4,28 @@ const app = express();
 const PORT = process.env.PORT;
 const { searchProducts } = require('./SerpAPIService');
 
-app.get('/search', async (req, res) => {
+app.get('/search', async (req, res) => 
+{
     const query = req.query.q;
-    if (!query) {
-        return res.status(400).send("Query parameter 'q' is required");
+    if (!query) 
+    {
+        return res.status(400).send("El parámetro 'q' dentro del query no está especificado");
     }
-    try {
+
+    try 
+    {
         const products = await searchProducts(query);
         res.json(products);
-    } catch (error) {
+    } 
+    
+    catch (error)
+    {
         console.error(error);
         res.status(500).send("Error al buscar productos");
     }
 });
 
-app.listen(PORT, () => {
+app.listen(PORT, () => 
+{
     console.log(`Servidor corriendo en http://localhost:${PORT}`);
 });
