a93bff52579fff8e4da0ac646a0531692a8a35a8
Alejos011 <alejandro.gonzalez.flme011@gmail.com>
2024-11-09 12:40:02 -0600

    indentation fixed

--- SerpAPIService.js
+++ SerpAPIService.js
@@ -1,8 +1,11 @@
 const { getJson } = require("serpapi");
 
-async function searchProducts(query) {
-    return new Promise((resolve, reject) => {
+async function searchProducts(query) 
+{
+    return new Promise((resolve, reject) => 
+    {
         getJson({
+            
             engine: "google_shopping",
             q: query,
             location: "Ciudad de México, México",
@@ -10,11 +13,18 @@ async function searchProducts(query) {
             gl: "mx",
             google_domain: "google.com.mx",
             api_key: process.env.SERP_API_KEY
-        }, (json) => {
-            if (json.error) {
+        }
+        ,(json) => 
+        {
+            if (json.error)
+            {
                 reject(json.error);
-            } else {
+            } 
+            
+            else 
+            {
                 const structuredResults = json.shopping_results.map(product => ({
+                    
                     title: product.title,
                     link: product.link,
                     price: product.price,
@@ -23,6 +33,7 @@ async function searchProducts(query) {
                     reviews: product.reviews || 0,
                     source: product.source,
                     shipping: product.additional_price?.shipping || "No disponible",
+                    
                 }));
 
                 resolve(structuredResults);
